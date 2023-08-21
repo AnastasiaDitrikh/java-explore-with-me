0db91eb1b536b0d02284748e6b0b02ad3811d8eb
Nastya <nastya.pgn081@gmail.com>
2023-08-21 19:50:54 +0300

    "Реализация второго этапа ДП: Fix unused import"

--- main-service/src/main/java/ru/practicum/ewm/model/mappers/CompilationMapper.java
+++ main-service/src/main/java/ru/practicum/ewm/model/mappers/CompilationMapper.java
@@ -3,7 +3,6 @@ package ru.practicum.ewm.model.mappers;
 import lombok.experimental.UtilityClass;
 import ru.practicum.ewm.dto.CompilationDto;
 import ru.practicum.ewm.dto.NewCompilationDto;
-import ru.practicum.ewm.dto.UpdateCompilationDto;
 import ru.practicum.ewm.model.Compilation;
 
 @UtilityClass
