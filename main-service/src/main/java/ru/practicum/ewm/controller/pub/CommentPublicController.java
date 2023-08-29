768e263f886b04497116957fd0774cd25b3091cb
Nastya <nastya.pgn081@gmail.com>
2023-08-29 22:57:03 +0300

    "Реализация третьего этапа ДП: Develop comments, final version"

--- main-service/src/main/java/ru/practicum/ewm/controller/pub/CommentPublicController.java
+++ main-service/src/main/java/ru/practicum/ewm/controller/pub/CommentPublicController.java
@@ -2,16 +2,14 @@ package ru.practicum.ewm.controller.pub;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-import ru.practicum.ewm.dto.comment.NewReportDto;
-import ru.practicum.ewm.dto.comment.ReportDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.practicum.ewm.model.Comment;
 import ru.practicum.ewm.service.CommentService;
-import ru.practicum.ewm.service.ReportService;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -22,18 +20,9 @@ import java.util.List;
 public class CommentPublicController {
     private final CommentService commentService;
 
-    private final ReportService reportService;
-
     @GetMapping("/{eventId}")
     public List<Comment> getRequestListAllCommentsEvent(@PathVariable Long eventId) {
         log.info("GET запрос на получение всех комментариев своего события с id = {} ", eventId);
         return commentService.getCommentEvent(eventId);
-    }
-
-    @PostMapping("/{commentId}/report")
-    @ResponseStatus(HttpStatus.CREATED)
-    public ReportDto addReport(@PathVariable Long commentId, @Valid @RequestBody NewReportDto reportDto) {
-        log.info("Опубликована жалоба на комментарий с id = {} ", commentId);
-        return reportService.addReport(commentId, reportDto);
     }
 }
--- main-service/src/main/java/ru/practicum/ewm/model/enums/ReportStatus.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.ewm.model.enums;
-
-public enum ReportStatus {
-    NEW,
-    REJECTED,
-    APPROVED;
-}
